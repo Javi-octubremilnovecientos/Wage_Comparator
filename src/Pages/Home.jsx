b02649460cfbfc2a03b580b6ab8157e6459815c8
Javier <javier.lombar2@gmail.com>
2025-05-28 12:35:53 +0200

    Cracionm de Surveys

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,30 @@ import React, { useState } from "react";
 import { MainForm } from "../Components/MainForm";
 
 import { Arrow } from "../assets/Icons/Icons";
-
+import {data} from "../Mocks/2-Grap-exported-final.js"
 export const Home = () => {
   
   const [started, setstarted] = useState(false);
+
+const datos = data;
+
+const europeos = datos.filter((dato) => 
+  ["Spain", "France", "Portugal", "Germany","Netherlands"].includes(dato.COUNTRY)
+);
+
+
+const salarios = europeos.map((vet) => {
+  
+  console.log(vet.WAGEGR1)
+ 
+});
+
+const totalSalario = salarios.reduce((acc, curr) => acc + curr, 0);
+
+ const media = totalSalario / salarios.length
+
+console.log(media);
+
 
   return (
     <main>
