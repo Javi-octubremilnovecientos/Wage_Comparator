7737f4b7433321f5023c6cfc6545175946624ebe
Javier <javier.lombar2@gmail.com>
2025-05-29 12:34:30 +0200

    requiered

--- src/Components/MainForm.jsx
+++ src/Components/MainForm.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../Styles/mainForm.css";
 import { surveys } from "../Mocks/Survey";
 export const MainForm = () => {
   const [formNo, setformNo] = useState(0);
   const [inputNo, setinputNo] = useState(0);
   const [form, setform] = useState(surveys[formNo]);
-const [valor, setValue] = useState()
+
+  const formRef = useRef(null);
 
 const HandleSubmit = (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const values = {};
   for (let [key, value] of formData.entries()) {
-    values[key] = value;
-  }
-  console.log(values); // Ahora sí imprime los valores
+       values[key] = value;
+    }
+    console.log(values)
+// Ahora sí imprime los valores
   setformNo(formNo + 1); // Avanza al siguiente formulario
   setinputNo(0); // Reinicia el inputNo
-  setValue(""); 
+  formRef.current.reset();
 };
 
 
@@ -29,14 +31,13 @@ const HandleSubmit = (e) => {
   const inputs = Object.values(form);
 
   return (
-<form className="main-form" onSubmit={HandleSubmit}>
+<form className="main-form"  ref={formRef} onSubmit={HandleSubmit}>
   {inputs &&
     inputs.map((input, i) => (
       <div className="inputController" key={i}>
         <label htmlFor="">{i <= inputNo ? input : ""}</label>
         <input
-          value={valor}
-          onChange={(e) => setValue(e.target.value)}
+        required
           name={input}
           type="text"
           disabled={i <= inputNo ? false : true}
